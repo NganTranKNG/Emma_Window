cc61c9b32e97c8c487187b43ec1cabb7d203947a
Ngan Tran <tranvanngan@kng.vn>
2022-09-16 09:48:55 +0700

    first commit

---
+++ HotKeys/HotKeys/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace HotKeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.edtCommandNote = new System.Windows.Forms.TextBox();
+            this.cbbSuggestCommand = new System.Windows.Forms.ComboBox();
+            this.edtCommand = new System.Windows.Forms.TextBox();
+            this.edtInputHotKey = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lblNote);
+            this.splitContainer1.Panel2.Controls.Add(this.edtCommandNote);
+            this.splitContainer1.Panel2.Controls.Add(this.cbbSuggestCommand);
+            this.splitContainer1.Panel2.Controls.Add(this.edtCommand);
+            this.splitContainer1.Panel2.Controls.Add(this.edtInputHotKey);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSave);
+            this.splitContainer1.Panel2.Controls.Add(this.btnRemove);
+            this.splitContainer1.Size = new System.Drawing.Size(563, 450);
+            this.splitContainer1.SplitterDistance = 301;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.AutoArrange = false;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(301, 450);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblNote
+            // 
+            this.lblNote.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNote.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblNote.Location = new System.Drawing.Point(0, 0);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(258, 94);
+            this.lblNote.TabIndex = 6;
+            this.lblNote.Text = "sdf sd fds sf \r\nsdfsdf\r\nsdfsdf";
+            this.lblNote.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // edtCommandNote
+            // 
+            this.edtCommandNote.Location = new System.Drawing.Point(2, 184);
+            this.edtCommandNote.Multiline = true;
+            this.edtCommandNote.Name = "edtCommandNote";
+            this.edtCommandNote.Size = new System.Drawing.Size(252, 137);
+            this.edtCommandNote.TabIndex = 5;
+            // 
+            // cbbSuggestCommand
+            // 
+            this.cbbSuggestCommand.FormattingEnabled = true;
+            this.cbbSuggestCommand.Location = new System.Drawing.Point(2, 155);
+            this.cbbSuggestCommand.Name = "cbbSuggestCommand";
+            this.cbbSuggestCommand.Size = new System.Drawing.Size(252, 23);
+            this.cbbSuggestCommand.TabIndex = 4;
+            // 
+            // edtCommand
+            // 
+            this.edtCommand.Location = new System.Drawing.Point(3, 126);
+            this.edtCommand.Name = "edtCommand";
+            this.edtCommand.Size = new System.Drawing.Size(252, 23);
+            this.edtCommand.TabIndex = 3;
+            // 
+            // edtInputHotKey
+            // 
+            this.edtInputHotKey.Location = new System.Drawing.Point(3, 97);
+            this.edtInputHotKey.Name = "edtInputHotKey";
+            this.edtInputHotKey.Size = new System.Drawing.Size(252, 23);
+            this.edtInputHotKey.TabIndex = 2;
+            this.edtInputHotKey.TextChanged += new System.EventHandler(this.edtInput_TextChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 389);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(6, 389);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 0;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox edtInputHotKey;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox edtCommandNote;
+        private System.Windows.Forms.ComboBox cbbSuggestCommand;
+        private System.Windows.Forms.TextBox edtCommand;
+        private System.Windows.Forms.Label lblNote;
+    }
+}
+
---
+++ HotKeys/HotKeys/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SautinSoft.Document;
+using SautinSoft.Document.Drawing;
+//using Newtonsoft.Json;
+
+namespace HotKeys
+{
+    
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        ///  command list
+        /// </summary>
+        private const int WM_APPCOMMAND = 0x319;
+
+        private const int APPCOMMAND_R_MENU = 0xF000;
+        private const int APPCOMMAND_BROWSER_BACK = 0x10000;
+        private const int APPCOMMAND_BROWSER_FORWARD = 0x20000;
+        private const int APPCOMMAND_BROWSER_REFRESH = 0x30000;
+        private const int APPCOMMAND_BROWSER_STOP = 0x40000;
+        private const int APPCOMMAND_BROWSER_SEARCH = 0x50000;
+        private const int APPCOMMAND_BROWSER_FAVORITE = 0x60000;
+        private const int APPCOMMAND_BROWSER_HOME = 0x70000;
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_MEDIA_NEXT = 0xB0000;
+        private const int APPCOMMAND_MEDIA_PREVIOUS = 0xC0000;
+        private const int APPCOMMAND_MEDIA_STOP = 0xD0000;
+        private const int APPCOMMAND_MEDIA_PAUSE = 0xE0000;
+
+        /// <summary>
+        ///  config
+        /// </summary>
+        private const string configFileName = "hotKey.cfg";
+
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private class KeyModifier
+        {
+            public static int None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            /// load config
+            readConfig();
+            registHotKey();
+        }
+
+        /**
+         * run in application
+         */
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.L))
+            {
+                //MessageBox.Show("What the Ctrl+F?");
+                
+                Keys key = (Keys)(((int)msg.LParam >> 16) & 0xFFFF);
+                int modifier = ((int)msg.LParam & 0xFFFF);
+                int id = msg.WParam.ToInt32();
+                lblNote.Text = "=>" + key.GetHashCode() + " - " + modifier.GetHashCode() + " = " + id;
+                if(id > 0) {
+                    keyData = Keys.A;
+                    //SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+                    //return true;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        /**
+         * listener key down
+         */
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                int modifier = ((int)m.LParam & 0xFFFF);
+                int id = m.WParam.ToInt32();
+
+                lblNote.Text = "=>" + key.GetHashCode() + " - " + modifier.GetHashCode() + " = " + id;
+
+                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)id);
+               /* switch (id)
+                {
+                    case 0:
+                        SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+                        return;
+                    case 1:
+                        SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+                        return;
+                    case 2:
+                        SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+                        return;
+                }*/
+
+                //MessageBox.Show("Hotkey hasNot been pressed!");
+                // do something
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            saveConfig();
+        }
+
+        private void edtInput_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);       // Unregister hotkey with id 0 before closing the form. You might want to call this more than once with different id values if you are planning to register more than one hotkey.
+        }
+
+
+        /// <summary>
+        /// hotkey
+        /// </summary>
+        
+        private void registHotKey()
+        {
+            // The id of the hotkey. 0 / 1
+
+            /*APPCOMMAND_VOLUME_MUTE = 0x80000; 173
+            APPCOMMAND_VOLUME_DOWN = 0x90000;
+            APPCOMMAND_VOLUME_UP = 0xA0000;
+            APPCOMMAND_MEDIA_NEXT = 0xB0000;
+            APPCOMMAND_MEDIA_PREVIOUS = 0xC0000;
+            APPCOMMAND_MEDIA_STOP = 0xD0000;
+            APPCOMMAND_MEDIA_PAUSE = 0xF0000;*/
+
+            // work
+            registHotKey(APPCOMMAND_VOLUME_MUTE, KeyModifier.Control, Keys.Multiply.GetHashCode());
+            registHotKey(APPCOMMAND_VOLUME_UP, KeyModifier.Control, Keys.Add.GetHashCode());
+            registHotKey(APPCOMMAND_VOLUME_DOWN, KeyModifier.Control, Keys.Subtract.GetHashCode());
+            registHotKey(APPCOMMAND_MEDIA_NEXT, KeyModifier.Control, Keys.NumPad6.GetHashCode());
+            registHotKey(APPCOMMAND_MEDIA_PREVIOUS, KeyModifier.Control, Keys.NumPad4.GetHashCode());
+            registHotKey(APPCOMMAND_MEDIA_STOP, KeyModifier.Control, Keys.NumPad8.GetHashCode());
+            registHotKey(APPCOMMAND_MEDIA_PAUSE, KeyModifier.Control, Keys.NumPad5.GetHashCode());
+
+            /// not work
+            registHotKey(APPCOMMAND_BROWSER_HOME, KeyModifier.Control, Keys.NumPad2.GetHashCode());
+            registHotKey(APPCOMMAND_BROWSER_BACK, KeyModifier.Control, Keys.NumPad1.GetHashCode());
+            registHotKey(APPCOMMAND_BROWSER_FORWARD, KeyModifier.Control, Keys.NumPad3.GetHashCode());
+        }
+
+        private void registHotKey(int id, int modifier, int hashCode)
+        {
+            RegisterHotKey(this.Handle, id, modifier, hashCode);
+        }
+
+        /// <summary>
+        /// area config file
+        /// </summary>
+
+        private void saveConfig()
+        {
+            using (var stream = File.Open(configFileName, FileMode.Create))
+            {
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    writer.Write(convertDataToJson());
+                }
+            }
+        }
+
+        private void readConfig()
+        {
+            if (File.Exists(configFileName))
+            {
+                using (var stream = File.Open(configFileName, FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        convertJsonToData(reader.ReadString());
+                    }
+                }
+            }
+        }
+
+        private void convertJsonToData(string data)
+        {
+
+            int muteHash = Keys.VolumeMute.GetHashCode();// = 173, 0x80000; 
+            int volDownHash = Keys.VolumeDown.GetHashCode();// = 174, 0x90000;
+            int volUpHash = Keys.VolumeUp.GetHashCode();// = 175, 0xA0000;
+            int trackNextHash = Keys.MediaNextTrack.GetHashCode();// = 176, 0xB0000;
+            int trackPreviousHash = Keys.MediaPreviousTrack.GetHashCode();// = 177, 0xC0000;
+            int stopHash= Keys.MediaStop.GetHashCode();// = 178, 0xD0000;
+            int pauseHash = Keys.MediaPlayPause.GetHashCode();// = 179, 0xE0000;
+
+            lblNote.Text = data
+                + "\n"
+                + APPCOMMAND_VOLUME_MUTE + " / " + APPCOMMAND_VOLUME_UP + " / " + APPCOMMAND_VOLUME_DOWN
+                + " \n "
+                + (muteHash & 0xFFFF) + " / " + convertIntToHex(volUpHash) + " / " + (volDownHash).ToString("X4");
+            
+        }
+
+        private string convertDataToJson()
+        {
+            return "";
+        }
+        
+        private int convertIntToHex(int intValue)
+        {
+            // Convert integer 182 as a hex in a string variable
+            string hexValue = intValue.ToString("X");
+            // Convert the hex string back to the number
+            int intAgain = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+            return intAgain;
+        }
+
+        private void removeAttribute_title()
+        {
+            string[] filePaths = Directory.GetFiles("C:\\Users\\tranvanngan\\Downloads\\Video", "*.*",SearchOption.AllDirectories).ToList();
+            foreach (string filePath in filePaths)
+            {
+                 if (File.Exists(filePath))
+                {
+                    var fileInfo = new FileInfo(filePath);
+                    FileAttributes attributes = fileInfo.Attributes;
+                    FileStream fs = File.Create(filePath);
+                }
+            }
+
+        }
+    }
+}
